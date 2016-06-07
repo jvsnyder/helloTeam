3119e9070df07b35c1de652c163468055433a5de
Jason Snyder <jvsnyder15@gmail.com>
2016-06-06 23:16:54 -0700

    TEAM-1 Printed Hello with every team members name

--- helloTeam.php
+++ helloTeam.php
@@ -3,7 +3,15 @@
 
 //The team is coming into town. Say hello to everyone on the team!
 
-$teamMemberName = array("Claudia", "Chris K", "Chris S", "Eric", "Sean", "Shweta", "Srini", "BIG_Thomas!");
+$teamMemberName = array("Chris K", "Chris S", "Claudia", "Eric", "Jason", "Sean", "Shweta", "Srini", "BIG_Thomas!");
 
-echo "Hello";
+echo ("Hello ". $teamMemberName[0]. "<br>");
+echo ("Hello ". $teamMemberName[1]. "<br>");
+echo ("Hello ". $teamMemberName[2]. "<br>");
+echo ("Hello ". $teamMemberName[3]. "<br>");
+echo ("Hello ". $teamMemberName[4]. "<br>");
+echo ("Hello ". $teamMemberName[5]. "<br>");
+echo ("Hello ". $teamMemberName[6]. "<br>");
+echo ("Hello ". $teamMemberName[7]. "<br>");
+echo ("Hello ". $teamMemberName[8]. "<br>");
 ?>
