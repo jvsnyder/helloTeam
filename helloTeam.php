5dc76f711659c8bb987eb174e12386c6509bf409
Jason Snyder <jvsnyder15@gmail.com>
2016-06-06 20:45:57 -0700

    first commit

---
+++ helloTeam.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//The team is coming into town. Say hello to everyone on the team!
+
+$teamMemberName = array("Claudia", "Chris K", "Chris S", "Eric", "Sean", "Shweta", "Srini", "BIG_Thomas!");
+
+echo "Hello";
+?>
