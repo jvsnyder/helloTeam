a16fd60e044ede310dafc014e41c3c15c64a1c4f
Jason Snyder <jvsnyder15@gmail.com>
2016-06-07 08:22:23 -0700

    TEAM-1 fixed coding standard DRY issue

--- helloTeam.php
+++ helloTeam.php
@@ -1,17 +1,17 @@
 <?php
 
+function greetTeamMember($name)
+{
+	echo ("Hello ". $name . "<br>");
+}
 
 //The team is coming into town. Say hello to everyone on the team!
 
 $teamMemberName = array("Chris K", "Chris S", "Claudia", "Eric", "Jason", "Sean", "Shweta", "Srini", "BIG_Thomas!");
 
-echo ("Hello ". $teamMemberName[0]. "<br>");
-echo ("Hello ". $teamMemberName[1]. "<br>");
-echo ("Hello ". $teamMemberName[2]. "<br>");
-echo ("Hello ". $teamMemberName[3]. "<br>");
-echo ("Hello ". $teamMemberName[4]. "<br>");
-echo ("Hello ". $teamMemberName[5]. "<br>");
-echo ("Hello ". $teamMemberName[6]. "<br>");
-echo ("Hello ". $teamMemberName[7]. "<br>");
-echo ("Hello ". $teamMemberName[8]. "<br>");
+foreach($teamMemberName as $name)
+{
+ 	greetTeamMember($name);
+}
+
 ?>
